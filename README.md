cc763afe38d8001096033a5ff81fb5cc3695f860
NimSum <nimm.garcia@gmail.com>
2022-03-28 16:59:51 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-dynamic-tabs-ppd2hl
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-dynamic-tabs-ppd2hl)
---
+++ app/app.component.ts
@@ -0,0 +1,25 @@
+//our root app component
+import { Component, ViewChild } from '@angular/core';
+
+import { TabsComponent } from './tabs/tabs.component';
+
+@Component({
+  selector: 'my-app',
+  template: `
+    <my-tabs>
+      <my-tab [tabTitle]="'Tab1'">
+          <p>OLA</p>
+          <p>Tab1 Content</p>
+          <p>AAAAAAAAAAAA</p>
+      </my-tab>
+      <my-tab [tabTitle]="'Tab2'">
+          <p>Tab2 Content</p>
+      </my-tab>
+    </my-tabs>
+  `,
+})
+export class AppComponent {
+  @ViewChild('personEdit') editPersonTemplate;
+  @ViewChild('about') aboutTemplate;
+  @ViewChild(TabsComponent) tabsComponent;
+}
